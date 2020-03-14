691bd9cd76d64e76f63fdee7cab85ac61619d54c
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-14 16:33:39 +0330

    Add lobbies endpoints.

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import auth
+from app.api.endpoints import lobby
 
 api_router = APIRouter()
 api_router.include_router(auth.router, tags=["auth"])
+api_router.include_router(lobby.router, tags=["lobby"])
