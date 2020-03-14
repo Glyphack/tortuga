4c7a13f14b2a26f5138e318840441da788803571
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-14 21:42:05 +0330

    :wrench: cors origin middleware.

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.core import config
 from app.api.api_v1 import api_router
 
 app = FastAPI()
+
+# CORS
+origins = []
+
+# Set all CORS enabled origins
+if config.BACKEND_CORS_ORIGINS:
+    origins_raw = config.BACKEND_CORS_ORIGINS.split(",")
+    for origin in origins_raw:
+        use_origin = origin.strip()
+        origins.append(use_origin)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    ),
+print(origins)
 
 
 @app.get("/")
