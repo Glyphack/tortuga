10b986567fa9265010b539104891adf053258d2a
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-14 12:42:34 +0330

    :bug: fix not setting username in jwt token payload.

--- authentication.py
+++ authentication.py
@@ -27,6 +27,6 @@ async def login_for_access_token(user: User):
     access_token_expires = timedelta(
         minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"username": user.username}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
