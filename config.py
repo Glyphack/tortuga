282cd621e1df09c642c3258d78e6cf5535aa041d
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-14 11:31:45 +0330

    Add config.

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+
+def getenv_boolean(var_name, default_value=False):
+    result = default_value
+    env_value = os.getenv(var_name)
+    if env_value is not None:
+        result = env_value.upper() in ("TRUE", "1")
+    return result
+
+
+API_V1_STR = "/api/v1"
+
+SECRET_KEY = os.getenvb(b"SECRET_KEY")
+if not SECRET_KEY:
+    SECRET_KEY = os.urandom(32)
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 10  # 60 minutes * 24 hours * 8 days = 8 days
