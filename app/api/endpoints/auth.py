4be30dc5dafc782854040acb3720cdbac6aa20b6
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-15 10:19:52 +0330

    :sparkles: Add user token to cookie.

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 from app.helpers.jwt_helper import create_access_token
 from app.core import config
@@ -10,10 +10,13 @@ router = APIRouter()
 
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(user: User):
+async def login_for_access_token(user: User, response: Response):
     access_token_expires = timedelta(
         minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"username": user.username}, expires_delta=access_token_expires
-    )
+    ).decode("utf-8")
+
+    response.set_cookie("token", access_token)
+
     return {"access_token": access_token, "token_type": "bearer"}
