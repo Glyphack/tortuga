53cf0250123bdcb6ca9b4e28e4d863aa3ecc226e
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-18 11:54:17 +0330

    :pencil2: add game endpoints for doc.

---
+++ app/api/endpoints/game.py
@@ -0,0 +1,20 @@
+from fastapi.routing import APIRouter
+from starlette.requests import Request
+
+from app.schemas.game import (
+    GetGameStatusRequest,
+    GetGameStatusResponse,
+    DoActionRequest,
+)
+
+router = APIRouter()
+
+
+@router.get("/game/my-game", response_model=GetGameStatusResponse)
+async def my_game(request: Request, get_game_status: GetGameStatusRequest):
+    pass
+
+
+@router.post("game/action")
+async def do_action(request: Request, action_request: DoActionRequest):
+    pass
