166f31054fa9766788ede74bb9e4974600ae7c8c
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-18 11:56:01 +0330

    :sparkle: Add game endpoints.

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from app.api.endpoints import auth
 from app.api.endpoints import lobby
+from app.api.endpoints import game
 
 api_router = APIRouter()
 api_router.include_router(auth.router, tags=["auth"])
 api_router.include_router(lobby.router, tags=["lobby"])
+api_router.include_router(game.router, tags=["game"])
