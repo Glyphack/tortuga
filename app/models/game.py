6a9681fb03f93ece6a8b44efc660a898e2b507aa
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-19 15:52:15 +0330

    :building_construction: Move Positions enum

--- app/models/game.py
+++ app/models/game.py
@@ -35,35 +35,6 @@ class Player:
     role: Optional[Role] = None
 
 
-class Positions(Enum):
-    FL1 = "fl_1"
-    FL2 = "fl_2"
-    FL3 = "fl_3"
-    FL4 = "fl_4"
-    FL5 = "fl_5"
-    FL_EN = "fl_en"
-    FL_FR = "fl_fr"
-    JR1 = "jr_1"
-    JR2 = "jr_2"
-    JR3 = "jr_3"
-    JR4 = "jr_4"
-    JR5 = "jr_5"
-    JR_EN = "jr_en"
-    JR_FR = "jr_fr"
-    TR1 = "tr_1"
-    TR2 = "tr_2"
-    TR3 = "tr_3"
-    TR4 = "tr_4"
-    TR5 = "tr_5"
-    TR6 = "tr_6"
-    TR7 = "tr_7"
-    TR8 = "tr_8"
-    TR9 = "tr_9"
-    TR_EN = "tr_en"
-    TR_FR = "tr_fr"
-    SP = "sp"
-
-
 @dataclass
 class Game:
     id: str
