1e9b81493cda2a92f80c4c7fceb29b578b06d244
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-21 01:18:06 +0430

    :ok_hand: Add payload to action handler class.

--- app/api/services/game_services/action_handlers/action_handler.py
+++ app/api/services/game_services/action_handlers/action_handler.py
@@ -1,17 +1,21 @@
 from abc import abstractmethod
 
 from app.models.game import Game
-from app.schemas.game_schema import Action
+from app.schemas.game_schema import Action, PayloadType
 
 
 class ActionHandler:
-    def __init__(self, game: Game, player: str, action: Action):
+    def __init__(
+            self,
+            game: Game,
+            player: str,
+            action: Action,
+            payload: PayloadType):
         self.game = game
         self.action = action
         self.player = player
+        self.payload = payload
 
     @abstractmethod
     def execute(self):
         raise NotImplemented
-
-
