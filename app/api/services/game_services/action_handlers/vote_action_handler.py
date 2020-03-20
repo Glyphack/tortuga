94d59f6b5f0e3d814a5ddccb7f2bb62ef77fc492
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-21 01:18:21 +0430

    :sparkle: Add vote action handler.

---
+++ app/api/services/game_services/action_handlers/vote_action_handler.py
@@ -0,0 +1,28 @@
+from app.api.services.game_services.action_handlers.action_handler import (
+    ActionHandler
+)
+from app.schemas import game_schema
+
+
+class VoteActionHandler(ActionHandler):
+    def execute(self):
+        assert (
+                self.game.last_action.action_type == game_schema.Action.ActionType.CALL_FOR_AN_ATTACK and
+                self.game.last_action.action_data.state == game_schema.State.InProgress
+        )
+        vote_card = self.game.players_info.get(self.player).vote_cards.pop(
+            self.payload.vote_card_index - 1
+        )
+        self.game.votes.fire += vote_card.fire
+        self.game.votes.water += vote_card.water
+        self.game.last_action.action_data.participating_players.remove(
+            self.player
+        )
+        print(self.game.votes)
+        if len(self.game.last_action.action_data.participating_players) == 0:
+            if self.game.votes.fire < self.game.votes.water:
+                self.game.last_action.action_data.state = game_schema.State.Success
+            else:
+                self.game.last_action.action_data.state = game_schema.State.Failed
+
+
