9a500deb311d1458eb184049d6129aed0aa00655
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-22 17:21:03 +0430

    Add move action handler.

--- app/api/services/game_services/action_handlers/__init__.py
+++ app/api/services/game_services/action_handlers/__init__.py
@@ -6,10 +6,12 @@ from .call_for_an_attack_action_handler import CallForAnAttackActionHandler
 from .put_chest_action_handler import PutChestActionHandler
 from .vote_action_handler import VoteActionHandler
 from .maroon_crew_to_tortuga import MaroonCrewActionHandler
+from .move_action_handler import MoveActionHandler
 
 handlers: Dict[Action.ActionType, Type[ActionHandler]] = {
     Action.ActionType.CALL_FOR_AN_ATTACK: CallForAnAttackActionHandler,
     Action.ActionType.VOTE: VoteActionHandler,
     Action.ActionType.PUT_CHEST: PutChestActionHandler,
-    Action.ActionType.MAROON_ANY_CREW_MATE_TO_TORTUGA: MaroonCrewActionHandler
+    Action.ActionType.MAROON_ANY_CREW_MATE_TO_TORTUGA: MaroonCrewActionHandler,
+    Action.ActionType.MOVE: MoveActionHandler
 }
