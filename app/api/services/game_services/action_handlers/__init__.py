8b33c4846e7dd4532a22f7c8d0f63d33889897b0
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-22 15:53:52 +0430

    Add maroon crew action handler.

--- app/api/services/game_services/action_handlers/__init__.py
+++ app/api/services/game_services/action_handlers/__init__.py
@@ -5,9 +5,11 @@ from .action_handler import ActionHandler
 from .call_for_an_attack_action_handler import CallForAnAttackActionHandler
 from .put_chest_action_handler import PutChestActionHandler
 from .vote_action_handler import VoteActionHandler
+from .maroon_crew_to_tortuga import MaroonCrewActionHandler
 
 handlers: Dict[Action.ActionType, Type[ActionHandler]] = {
     Action.ActionType.CALL_FOR_AN_ATTACK: CallForAnAttackActionHandler,
     Action.ActionType.VOTE: VoteActionHandler,
-    Action.ActionType.PUT_CHEST: PutChestActionHandler
+    Action.ActionType.PUT_CHEST: PutChestActionHandler,
+    Action.ActionType.MAROON_ANY_CREW_MATE_TO_TORTUGA: MaroonCrewActionHandler
 }
