4e4a9ed85fc7568ec1a0773ef624172d5902afd4
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-23 15:39:52 +0430

    Add call for mutiny action handler.

---
+++ app/api/services/game_services/action_handlers/call_for_mutiny_action_handler.py
@@ -0,0 +1,42 @@
+from app.schemas.game_schema import Positions, Action, CallForMutinyActionData, \
+    State
+from .action_handler import ActionHandler
+
+
+class CallForMutinyActionHandler(ActionHandler):
+    def execute(self):
+        player_position = self.game.players_position[self.player]
+        if player_position in Positions.jr_positions():
+            assert player_position == Positions.JR2
+            captain = self.game.get_jr_caption()
+        elif player_position in Positions.fd_positions():
+            assert player_position == Positions.FD2
+            captain = self.game.get_fd_caption()
+        else:
+            raise AssertionError
+
+        action = Action(
+            action_type=Action.ActionType.CALL_FOR_A_MUTINY,
+            action_data=CallForMutinyActionData(
+                captain=captain,
+                participating_players=self._get_participating_players(),
+                state=State.InProgress
+            )
+        )
+
+        self.game.last_action = action
+
+    def _get_participating_players(self):
+        player_position = self.game.players_position[self.player]
+        if player_position in Positions.jr_positions():
+            positions = Positions.jr_positions()
+            positions.remove(Positions.JR1)
+        else:
+            positions = Positions.fd_positions()
+            positions.remove(Positions.FD1)
+        participating = []
+        for player, position in self.game.players_position.items():
+            if position in positions:
+                participating.append(player)
+
+        return participating
