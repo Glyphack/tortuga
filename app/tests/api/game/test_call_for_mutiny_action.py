d6cceebf40dd4a7d166cce07563aa5232f019142
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-23 15:39:21 +0430

    Add tests for call for mutiny.

---
+++ app/tests/api/game/test_call_for_mutiny_action.py
@@ -0,0 +1,29 @@
+from app.schemas.game_schema import Positions
+from .base import BaseGameTestCase
+
+
+class TestCallForMutiny(BaseGameTestCase):
+    def test_call_for_mutiny_starts_voting(self):
+        player = self.game.players[2]
+        self._call_for_mutiny_action(player)
+        response = self._get_my_game(player).json()
+        assert response["gameStatus"]["lastAction"]["actionData"][
+                   "participatingPlayers"] == [player]
+
+    def test_voting_on_mutiny(self):
+        player = self.game.players[2]
+        self._call_for_mutiny_action(player)
+        self._vote(player)
+        response = self._get_my_game(player).json()
+        assert (
+                response["gameStatus"]["lastAction"]["actionData"]["state"] in
+                ["success", "failed"]
+        )
+
+    def test_mutiny_success(self):
+        captain = self.game.players[0]
+        player = self.game.players[2]
+        self._call_for_mutiny_action(player)
+        self.game.players_info[player].vote_cards[0].skull += 1000
+        self._vote(player)
+        assert self.game.players_position[captain] == Positions.TR1
