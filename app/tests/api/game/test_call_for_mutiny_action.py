393ec41261461de1220b98175cd495bd36d607d1
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-24 11:43:15 +0430

    Update tests and obey game turns.

--- app/tests/api/game/test_call_for_mutiny_action.py
+++ app/tests/api/game/test_call_for_mutiny_action.py
@@ -5,6 +5,7 @@ from .base import BaseGameTestCase
 class TestCallForMutiny(BaseGameTestCase):
     def test_call_for_mutiny_starts_voting(self):
         player = self.game.players[2]
+        self.game.turn = player
         self._call_for_mutiny_action(player)
         response = self._get_my_game(player).json()
         assert response["gameStatus"]["lastAction"]["actionData"][
@@ -12,6 +13,7 @@ class TestCallForMutiny(BaseGameTestCase):
 
     def test_voting_on_mutiny(self):
         player = self.game.players[2]
+        self.game.turn = player
         self._call_for_mutiny_action(player)
         self._vote(player)
         response = self._get_my_game(player).json()
@@ -23,6 +25,7 @@ class TestCallForMutiny(BaseGameTestCase):
     def test_mutiny_success(self):
         captain = self.game.players[0]
         player = self.game.players[2]
+        self.game.turn = player
         self._call_for_mutiny_action(player)
         self.game.players_info[player].vote_cards[0].skull += 1000
         self._vote(player)
