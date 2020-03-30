c6562ff0018534bc46cdc6e93e300003bfe14a1e
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-30 15:36:56 +0430

    Fix player because turn changes.

--- app/tests/api/game/test_view_two_event_cards_action.py
+++ app/tests/api/game/test_view_two_event_cards_action.py
@@ -4,21 +4,22 @@ from .base import BaseGameTestCase
 
 class TestViewTwoEventCardsAction(BaseGameTestCase):
     def test_player_see_event_cards(self):
-        response = self._view_two_event_cards_action(self.game.turn, [1, 2])
+        player = self.game.turn
+        response = self._view_two_event_cards_action(player , [1, 2])
         assert response.status_code == 200
-        response = self._get_my_game(self.game.turn).json()
-
+        response = self._get_my_game(player).json()
         assert (
                 len(response["gameStatus"]["playerGameInfo"][
                         "seenEventCards"]) == 2
         )
 
     def test_view_correct_cards(self):
+        player = self.game.turn
         first_card = EventCardsManager.get(self.game.event_cards[0])
         second_card = EventCardsManager.get(self.game.event_cards[1])
-        response = self._view_two_event_cards_action(self.game.turn, [1, 2])
+        response = self._view_two_event_cards_action(player, [1, 2])
         assert response.status_code == 200
-        response = self._get_my_game(self.game.turn).json()
+        response = self._get_my_game(player).json()
 
         assert (
                 response["gameStatus"]["playerGameInfo"][
