d8d4b6add03351fe6e415e6a24d35dc4fa36c71a
glyphack <sh.hooshyari@cafebazaar.ir>
2020-03-31 10:52:31 +0430

    Update tests.

--- app/tests/api/game/test_move_treasure_action.py
+++ app/tests/api/game/test_move_treasure_action.py
@@ -4,19 +4,20 @@ from .base import BaseGameTestCase
 
 class TestMoveTreasureAction(BaseGameTestCase):
     def test_move_treasure_moves(self):
+        self.game.turn = self.game.players[2]
         player = self.game.turn
-        self.game.players_position[player] = Positions.JR1
         self.game.chests_position.jr_fr = 1
-        self._move_treasure_action(player, TreasureHoldTeams.france)
+        self._move_treasure_action(player, TreasureHoldTeams.france).json()
         request = self._get_my_game(player).json()
         assert request["gameStatus"]["chestsPosition"]["jr_en"] == 1
         assert request["gameStatus"]["chestsPosition"]["jr_fr"] == 0
 
     def test_move_treasure_changes_turn(self):
+        self.game.turn = self.game.players[2]
         player = self.game.turn
-        self.game.players_position[player] = Positions.JR1
         self.game.chests_position.jr_fr = 1
-        self._move_treasure_action(player, TreasureHoldTeams.france)
+        r = self._move_treasure_action(player, TreasureHoldTeams.france)
+        print(r.json())
         assert self.game.turn != player
 
     def test_move_from_empty_hold(self):
