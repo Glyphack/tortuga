62ee6c9ea13e5b81438abed8c51855678c874e9d
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-01 20:16:57 +0430

    Add event card handler base class.

--- app/api/services/game_services/action_handlers/event_card_handlers/event_card_handler.py
+++ app/api/services/game_services/action_handlers/event_card_handlers/event_card_handler.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from app.models.game import Game
 from app.schemas.game_schema import PayloadType
 
@@ -8,5 +10,11 @@ class EventCardHandler:
         self.player = player
         self.payload = payload
 
-    def reveal_card(self):
+    def reveal(self):
+        raise NotImplemented
+
+    def options(self) -> List:
+        raise NotImplemented
+
+    def set_option(self):
         raise NotImplemented
