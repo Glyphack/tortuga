265335939e1b147788c8dcefb103cb19b52bcacc
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-02 11:37:34 +0430

    Implement UseEventCardActionHandler.

---
+++ app/api/services/game_services/action_handlers/use_event_card.py
@@ -0,0 +1,16 @@
+from app.schemas.game_schema import Action
+from .action_handler import ActionHandler
+from ..event_card_handlers import event_card_handler
+
+
+class UseEventCardActionHandler(ActionHandler):
+    def execute(self):
+        event_card = event_card_handler[self.payload.event_card_slug](
+            self.game, self.player, self.payload
+        )
+        event_card.set_option(self.payload.event_card_option_index - 1)
+        event_card.reveal()
+        self.game.last_action = Action(
+            action_type=Action.ActionType.USE_EVENT_CARD,
+        )
+        self.game.next_turn()
