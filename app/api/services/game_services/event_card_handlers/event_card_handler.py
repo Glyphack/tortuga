8dd0553a715c61eaafb0ad39c2ec2b3c0e6f0fd1
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-02 11:38:13 +0430

    Update event card handler interface.

--- app/api/services/game_services/event_card_handlers/event_card_handler.py
+++ app/api/services/game_services/event_card_handlers/event_card_handler.py
@@ -1,3 +1,4 @@
+import abc
 from typing import List
 
 from app.models.game import Game
@@ -9,12 +10,21 @@ class EventCardHandler:
         self.game = game
         self.player = player
         self.payload = payload
+        self.option = None
 
+    @abc.abstractmethod
     def reveal(self):
         raise NotImplemented
 
+    def set_option(self, option):
+        self.option = self.options[option]
+
+    @property
+    @abc.abstractmethod
     def options(self) -> List:
         raise NotImplemented
 
-    def set_option(self):
-        raise NotImplemented
+    @property
+    @abc.abstractmethod
+    def can_keep(self):
+        return False
