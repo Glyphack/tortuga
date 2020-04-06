adb60fd6fd0e40d8802bfac4e4d9f40be82bd9d9
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-06 12:57:51 +0430

    Add can use for usable event cards.

--- app/api/services/game_services/event_card_handlers/letter_of_marque_handler.py
+++ app/api/services/game_services/event_card_handlers/letter_of_marque_handler.py
@@ -6,6 +6,18 @@ from .event_card_handler import EventCardHandler
 
 
 class LetterOfMarque(EventCardHandler):
+    @property
+    def can_use(self):
+        positions = [Positions.JR_B, Positions.FD_B]
+        positions.extend(Positions.tr_positions())
+        can_use = False
+        for player in self.game.players:
+            if player == self.player:
+                continue
+            if self.game.players_position[player] in positions:
+                can_use = True
+        return can_use
+
     def reveal(self):
         option_opr = self.options_operations[self.chosen_option]
         self.game.set_position(option_opr.who, option_opr.where)
