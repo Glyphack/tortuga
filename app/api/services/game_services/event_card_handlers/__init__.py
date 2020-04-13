8edf757632fd88ce8d9555397876cf66393cfadf
Glyphack <sh.hooshyari@gmail.com>
2020-04-13 18:02:04 +0430

    :sparkle: Pistol event card (#28)
    * Add pistol card
    
    * Create pistol card handler
    
    * Add test for pistol event card

--- app/api/services/game_services/event_card_handlers/__init__.py
+++ app/api/services/game_services/event_card_handlers/__init__.py
@@ -1,10 +1,12 @@
 from typing import Dict, Type
 
 from .event_card_handler import EventCardHandler
+from .pistol_handler import Pistol
 from .spanish_armada import SpanishArmadaCard
 from .letter_of_marque_handler import LetterOfMarque
 
 event_card_handler: Dict[str, Type[EventCardHandler]] = {
     "spanish-armada": SpanishArmadaCard,
-    "letter-of-marque": LetterOfMarque
+    "letter-of-marque": LetterOfMarque,
+    "pistol": Pistol
 }
---
+++ app/tests/api/game/event_cards/test_pistol.py
@@ -0,0 +1,20 @@
+from app.models.game import Game
+from app.schemas.game_schema import Positions
+from app.tests.api.game.base import BaseGameTestCase
+
+
+class TestPistolEventCard(BaseGameTestCase):
+    def test_reveal_pistol_card(self, game: Game):
+        game.event_cards = ["pistol"]
+        player = game.turn
+        self._reveal_event_card_action(player, 1)
+        game_response = self._get_my_game(player).json()
+        assert (
+                game_response["gameStatus"]["lastAction"][
+                    "actionType"] == "reveal one event card"
+        )
+        assert len(game_response["gameStatus"]["lastAction"][
+                       "actionData"]["eventCardOptions"]) == 3
+        self.use_event_card_action(player, "pistol", 0)
+        assert list(game.players_position.values())[
+                   1] in Positions.tr_positions()
