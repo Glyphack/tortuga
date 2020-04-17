607040e35a634af8d3ff99f721017ff4c3c7743b
Glyphack <sh.hooshyari@gmail.com>
2020-04-17 15:01:05 +0430

    :sparkle: Black spot evnet card (#29)
    * Add black spot event card data
    
    * add black spot event card handler
    
    * add tests for black spot
    
    * check player turn changes after revealing

---
+++ app/tests/api/game/event_cards/test_black_spot.py
@@ -0,0 +1,11 @@
+from app.schemas.game_schema import Positions
+from app.tests.api.game.base import BaseGameTestCase
+
+
+class TestBlackSpot(BaseGameTestCase):
+    def test_reveal_black_spot(self, game):
+        player = game.turn
+        game.event_cards = ["black-spot"]
+        self._reveal_event_card_action(player, 1)
+        assert game.players_position[player] in Positions.tr_positions()
+        assert game.turn != player
