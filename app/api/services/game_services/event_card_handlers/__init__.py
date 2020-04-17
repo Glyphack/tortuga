b47506ce3d0d21470760c301461b8e8f70c84fbf
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-17 14:55:46 +0430

    add black spot event card handler

--- app/api/services/game_services/event_card_handlers/__init__.py
+++ app/api/services/game_services/event_card_handlers/__init__.py
@@ -1,5 +1,6 @@
 from typing import Dict, Type
 
+from .black_spot import BlackSpot
 from .event_card_handler import EventCardHandler
 from .pistol_handler import Pistol
 from .spanish_armada import SpanishArmadaCard
@@ -8,5 +9,6 @@ from .letter_of_marque_handler import LetterOfMarque
 event_card_handler: Dict[str, Type[EventCardHandler]] = {
     "spanish-armada": SpanishArmadaCard,
     "letter-of-marque": LetterOfMarque,
-    "pistol": Pistol
+    "pistol": Pistol,
+    "black-spot": BlackSpot
 }
---
+++ app/api/services/game_services/event_card_handlers/black_spot.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from app.schemas.game_schema import Positions
+from .event_card_handler import EventCardHandler
+
+
+class BlackSpot(EventCardHandler):
+    def reveal(self):
+        self.game.set_position(self.player, Positions.TR)
+
+    @property
+    def options(self) -> List:
+        return []
+
+    @property
+    def can_keep(self):
+        return False
+
+    @property
+    def can_use(self):
+        return True
