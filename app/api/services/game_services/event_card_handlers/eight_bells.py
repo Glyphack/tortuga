baedde530f55f83abe02fc34c528b5226bd2dc83
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-23 21:16:11 +0430

    fix not giving players new position

--- app/api/services/game_services/event_card_handlers/eight_bells.py
+++ app/api/services/game_services/event_card_handlers/eight_bells.py
@@ -1,5 +1,5 @@
 import random
-from typing import List, Optional, Set
+from typing import List, Optional, Tuple
 
 from app.schemas.game_schema import Positions
 from .event_card_handler import EventCardHandler
@@ -8,26 +8,31 @@ from .event_card_handler import EventCardHandler
 class EightBells(EventCardHandler):
     def reveal(self) -> None:
         players = self.affected_players
+        move_to = self.affected_positions[1]
         random.shuffle(players)
+        print(self.game.players_position)
+        for player in self.affected_players:
+            print(self.game.players_position)
+            del self.game.players_position[player]
         for player in players:
-            self.game.set_position(player, )
+            self.game.set_position(player, move_to)
 
     @property
-    def affected_players(self) -> Set:
+    def affected_players(self) -> List:
         players = set()
-        for player, position in self.game.players_position:
-            if position in self.affected_positions:
+        for player, position in self.game.players_position.items():
+            if position in self.affected_positions[0]:
                 players.add(player)
-        return players
+        return list(players)
 
     @property
-    def affected_positions(self) -> Optional[List]:
+    def affected_positions(self) -> Optional[Tuple[List, Positions]]:
         if self.game.get_position(self.player) in Positions.jr_positions():
-            position = Positions.jr_positions()
+            position = (Positions.jr_positions(), Positions.JR)
         elif self.game.get_position(self.player) in Positions.fd_positions():
-            position = Positions.fd_positions()
+            position = (Positions.fd_positions(), Positions.FD)
         elif self.game.get_position(self.player) in Positions.tr_positions():
-            position = Positions.tr_positions()
+            position = (Positions.tr_positions(), Positions.TR)
         else:
             return None
         return position
