b8b11b1284e976fd2d38203c3253cc3a2a95f8dc
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-23 21:15:30 +0430

    add test for eight bells

---
+++ app/tests/api/game/event_cards/test_eight_bells.py
@@ -0,0 +1,15 @@
+from app.models.game import Game
+from app.tests.api.game.base import BaseGameTestCase
+
+
+class TestEightBells(BaseGameTestCase):
+    def test_reveal_eight_bells(self, game: Game):
+        player = game.turn
+        position = game.get_position(player)
+        # since it's random we test until we get the result!
+        while game.get_position(player) == position:
+            player = game.turn
+            position = game.get_position(player)
+            game.event_cards = ["eight-bells"]
+            self._reveal_event_card_action(player, 1)
+        assert game.get_position(player) != position
