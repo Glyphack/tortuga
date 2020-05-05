7eaddbf14d349f4a63d5cb92b240a24dc5e34f8d
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-06 00:12:58 +0430

    add fountain of youth event card

---
+++ app/api/services/game_services/event_card_handlers/fountain_of_youth.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from app.models.event_cards import EventCardsManager
+from .event_card_handler import EventCardHandler
+
+
+class FountainOfYouth(EventCardHandler):
+    def reveal(self):
+        self.game.get_player_info(self.player).event_cards.append(
+            EventCardsManager.get(self.slug)
+        )
+
+    @property
+    def options(self) -> List:
+        return []
+
+    @property
+    def options_operations(self) -> List:
+        return []
+
+    @property
+    def can_keep(self):
+        return False
+
+    @property
+    def can_use(self):
+        return False
+
+    @property
+    def slug(self):
+        return "fountain-of-youth"
