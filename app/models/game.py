aa0d4f96ba65e9ba05c4f63b404417b55536d380
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-06 00:23:09 +0430

    don't maroon player if player has fountain of youth event card

--- app/models/game.py
+++ app/models/game.py
@@ -157,9 +157,11 @@ class Game:
             fd_cabin_boy = p
         return [jr_cabin_boy, fd_cabin_boy]
 
-    def maroon_player(self, player_to_maroon) -> bool:
+    def maroon_player(self, player) -> bool:
+        if "fountain-of-youth" in self.get_player_info(player).event_cards:
+            return False
         self.set_position(
-            player_to_maroon,
+            player,
             Positions.TR
         )
         return True
