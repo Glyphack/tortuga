6c9b337b87b3bcf0d8ecbdb5ae10aea1f056218a
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-24 13:12:12 +0430

    change ActionType class to str, Enum

--- app/schemas/game_schema.py
+++ app/schemas/game_schema.py
@@ -171,7 +171,7 @@ class PutChestActionData(APIModel):
 
 
 class Action(APIModel):
-    class ActionType(Enum):
+    class ActionType(str, Enum):
         VIEW_TWO_EVENT_CARDS = "view two event cards"
         REVEAL_EVENT_CARD = "reveal one event card"
         FORCE_ANOTHER_PLAYER_TO_CHOOSE_CARD = (
