3c8311c9cbe066231660f58885732ff24f55d56c
Glyphack <sh.hooshyari@gmail.com>
2020-07-02 13:01:08 +0430

    fix can use for kept event cards

--- app/models/player.py
+++ app/models/player.py
@@ -1,13 +1,16 @@
 from dataclasses import field, dataclass
 from typing import List, Dict
 
+from app.api.services.game_services.event_card_handlers import (
+    event_card_handlers
+)
 from app.models.event_cards import EventCard, EventCardsManager
 from app.schemas.game_schema import Team, VoteCard, KeptEventCard
 
 
 @dataclass
 class Player:
-    id: str
+    username: str
     team: Team
     vote_cards: List[VoteCard] = field(default_factory=list)
     event_cards: List[str] = field(default_factory=list)
@@ -23,13 +26,17 @@ class Player:
     def has_event_card(self, slug) -> bool:
         return slug in self.event_cards
 
-    def get_kept_event_cards(self):
+    def get_kept_event_cards(self, game):
         kept_event_cards = []
         for event_card_slug in self.event_cards:
+            card_handler = event_card_handlers[
+                event_card_slug](
+                game=game, player=self.username
+            )
             kept_event_cards.append(
                 KeptEventCard(
                     event_card=EventCardsManager.get(event_card_slug),
-                    can_use=True
+                    can_use=card_handler.can_use
                 )
             )
-        return kept_event_cards
+            return kept_event_cards
