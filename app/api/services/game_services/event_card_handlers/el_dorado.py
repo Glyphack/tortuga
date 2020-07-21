2d86ccc4769b3f22cdafc8febfa1d11d1636f46e
glyphack <sh.hooshyari@gmail.com>
2020-07-22 00:29:59 +0430

    use can_vote function from game model

--- app/api/services/game_services/event_card_handlers/el_dorado.py
+++ app/api/services/game_services/event_card_handlers/el_dorado.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from .event_card_handler import EventCardHandler
-from app.api.services.game_services.service import can_vote
 
 
 class ElDorado(EventCardHandler):
@@ -29,7 +28,7 @@ class ElDorado(EventCardHandler):
     @property
     def can_use(self) -> bool:
         return (
-                can_vote(self.game, self.player) and
+                self.game.can_vote(self.player) and
                 self.game.get_player_info(
                     self.player
                 ).has_event_card(self.slug)
